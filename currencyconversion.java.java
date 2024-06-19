5bdf42de41ec655661c51d76a7a88cb9b3e6d064
vinaykumar-1nc22cd062 <shelbyyyy0502@gmail.com>
2024-06-19 11:21:21 +0530

    Add files via upload

---
+++ currencyconversion.java.java
@@ -0,0 +1,40 @@
+imoprt java.util.
+import text.decimal formulation;
+
+public class currencyconversion.
+
+{
+public static void main(String[]args)
+{
+amount.riggit;
+double amount.riggit;
+double amount.dollar.code.riggit;
+Decimal format F=new Decimal format
+@ suppress warings("resource")
+Scanner sc=new Scanner (system.in);
+System.out.println)"currencyconversion");
+System.out.println("which currency you want to convert?");
+System.out println(1:Dollar 1+2: ringgit");
+code=sc.next.int();
+System.new.println(How much mony you want to convert?");
+amount=sc.next Float();
+if(code==1)
+{
+ringgit=amount*4.17
+System.out.println("your"+ amount + "Dollar is:");
+if format(ringgit) + "ringgit");
+}
+else if (code==2)
+{
+ringgit=amount*4.17
+System.out.println("your" + amount "Dollar is :");
+if.format (ringgit) + "ringgit");
+}
+else
+{
+System.out.println("Invalid Input");
+}
+System.out.println("Thank you");
+}
+}
+
